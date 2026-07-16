d8f47b6e985023ee40bf04e1334e9f9f3cc95c81
rizanne-f <6rai21@gmail.com>
2026-07-16 09:34:37 +0800

    write logic to play a round

--- script.js
+++ script.js
@@ -15,4 +15,35 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("Choose between 'rock', 'paper', or 'scissors':")
 }
-console.log(getHumanChoice())
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return console.log(`It's a tie! Both players chose ${humanChoice}`)
+    } else if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            computerScore += 1
+            return console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+        }
+        humanScore += 1
+        return console.log(`You won! ${humanChoice} beats ${computerChoice}`)
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            computerScore += 1
+            return console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+        }
+        humanScore += 1
+        return console.log(`You won! ${humanChoice} beats ${computerChoice}`)
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            computerScore += 1
+            return console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+        }
+        humanScore += 1
+        return console.log(`You won! ${humanChoice} beats ${computerChoice}`)
+    }
+}
+
+const humanSelection = getHumanChoice().toLowerCase()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection);
